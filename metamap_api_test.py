7dacaef836787c68b59f978f6e845fe8b2d6cb16
Morgis, Matt (ELS-PHL) <m.morgis@elsevier.com>
2017-07-18 15:42:57 -0400

    test: testing multiple, one-word concepts in a sentence

--- metamap_api_test.py
+++ metamap_api_test.py
@@ -29,10 +29,6 @@ class MetamapAPITest(unittest.TestCase):
                          ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'])
 
     def test_singe_one_word_concept_in_sentence(self):
-        line = 'index|mm|score|preferred_name|C0011849|[dsyn]|trigger|location|10/8|tree_codes'
-        concept = self.buildConcept(line)
-        metamap = MetaMapMock([concept])
-        metamap_api = MetaMapAPI(metamap)
         sentence = {'text': 'John has diabetes.',
                     'char_offsets':
                         [0, 5, 9, 17],
@@ -40,6 +36,10 @@ class MetamapAPITest(unittest.TestCase):
                         ['O', 'O', 'O', 'O'],
                     'entity_cids':
                         ['O', 'O', 'O', 'O']}
+        mm_output = 'index|mm|score|preferred_name|C0011849|[dsyn]|trigger|location|10/8|tree_codes'
+        concepts = self.buildConcept(mm_output)
+        metamap = MetaMapMock(concepts)
+        metamap_api = MetaMapAPI(metamap)
 
         tagged_sentence = metamap_api.tag(sentence)
         self.assertEqual(tagged_sentence['entity_types'],
@@ -47,10 +47,33 @@ class MetamapAPITest(unittest.TestCase):
         self.assertEqual(tagged_sentence['entity_cids'],
                          ['O', 'O', 'C0011849', 'O'])
 
-    def buildConcept(self, line):
-        fields = line.split('|')
-        concept = ConceptMMI.from_mmi(line)
-        return concept
+    def test_triple_one_word_concept_in_sentence(self):
+        sentence = {'text': 'John has diabetes which causes hunger and fatigue.',
+                    'char_offsets':
+                        [0, 5, 9, 18, 24, 31, 38, 42, 49],
+                    'entity_types':
+                        ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+                    'entity_cids':
+                        ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']}
+        mm_output = """index|mm|score|preferred_name|C0011849|[dsyn]|trigger|location|10/8|tree_codes\nindex|mm|score|preferred_name|C0020175|[sosy]|trigger|location|32/6|tree_codes\nindex|mm|score|preferred_name|C0015672|[sosy]|trigger|location|43/7|tree_codes"""
+        concepts = self.buildConcept(mm_output)
+        metamap = MetaMapMock(concepts)
+        metamap_api = MetaMapAPI(metamap)
+
+        tagged_sentence = metamap_api.tag(sentence)
+        self.assertEqual(tagged_sentence['entity_types'],
+                         ['O', 'O', 'Disease', 'O', 'O', 'Symptom', 'O', 'Symptom', 'O'])
+        self.assertEqual(tagged_sentence['entity_cids'],
+                         ['O', 'O', 'C0011849', 'O', 'O', 'C0020175', 'O', 'C0015672', 'O'])
+
+    def buildConcept(self, mm_output):
+        concepts = []
+        lines = mm_output.splitlines()
+        for line in lines:
+            fields = line.split('|')
+            concept = ConceptMMI.from_mmi(line)
+            concepts.append(concept)
+        return concepts
 
 
 class MetaMapMock(object):

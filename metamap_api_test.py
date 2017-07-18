10a679ff6b2c21a009758742395a9a79e7f996e1
Morgis, Matt (ELS-PHL) <m.morgis@elsevier.com>
2017-07-18 09:21:41 -0400

    test: no concept in sent

--- metamap_api_test.py
+++ metamap_api_test.py
@@ -7,24 +7,56 @@ from metamap_api import MetaMapAPI
 class MetamapAPITest(unittest.TestCase):
 
     def test_no_concept_in_sentence(self):
-        metamap_api = MetaMapAPI(MetaMapMock)
-        metamap_api.tag('')
-        self.assertEqual(1, 3 - 2)
+        concept = []
+        metamap = MetaMapMock(concept)
+        metamap_api = MetaMapAPI(metamap)
+        sentence = {'text': 'Shoba went on a bike ride this weekend.',
+                    'char_offsets':
+                        [0, 6, 11, 14, 16, 21, 26, 31, 38],
+                    'entity_types':
+                        ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+                    'entity_cids':
+                        ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']}
+
+        tagged_sentence = metamap_api.tag(sentence)
+        self.assertEqual(tagged_sentence['entity_types'],
+                         ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'])
+        self.assertEqual(tagged_sentence['entity_cids'],
+                         ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'])
+
+    def test_singe_one_word_concept_in_sentence(self):
+        concept = []
+        metamap = MetaMapMock(concept)
+        metamap_api = MetaMapAPI(metamap)
+        sentence = {'text': 'John has diabetes.',
+                    'char_offsets':
+                        [0, 5, 9, 17],
+                    'entity_types':
+                        ['O', 'O', 'O', 'O'],
+                    'entity_cids':
+                        ['O', 'O', 'O', 'O']}
+
+        tagged_sentence = metamap_api.tag(sentence)
+        self.assertEqual(tagged_sentence['entity_types'],
+                         ['O', 'O', 'O', 'Disease'])
+        self.assertEqual(tagged_sentence['entity_cids'],
+                         ['O', 'O', 'O', 'C0011849'])
 
 
 class MetaMapMock(object):
 
-    def extract_concepts(self, sentences=None, ids=None,
-                         filename=None, composite_phrase=4,
-                         file_format='sldi', word_sense_disambiguation=True):
-        """ Extract concepts from a list of sentences using MetaMap. """
-        print('here')
-        return [['here'], '']
+    def __init__(self, concepts):
+        self.concepts = concepts
 
-    @staticmethod
-    def get_instance(metamap_filename, version=None, backend='subprocess',
-                     **extra_args):
-        return MetaMapMock()
+    def extract_concepts(self,
+                         sentences=None,
+                         ids=None,
+                         filename=None,
+                         composite_phrase=4,
+                         file_format='sldi',
+                         word_sense_disambiguation=True):
+        """ Extract concepts from a list of sentences using MetaMap. """
+        return (self.concepts, [])
 
 
 if __name__ == '__main__':
